0eaf7674d45a5ec28a0600f192b63fe527f6a89c
Govind Kumar Choudhary <govind.contact@gmail.com>
2018-06-01 20:28:45 +0530

    after adding POST, PUT, DELETE verbs of HTTP

--- SpringJPAPostgreSQLAngularJS/src/main/java/com/rank/ccms/controller/BankMstController.java
+++ SpringJPAPostgreSQLAngularJS/src/main/java/com/rank/ccms/controller/BankMstController.java
@@ -3,6 +3,11 @@ package com.rank.ccms.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,11 +28,23 @@ public class BankMstController {
 
 	}
 
-	@RequestMapping(value = "/bank", method = RequestMethod.POST, produces = { "application/json" })
-	public List<BankMst> addBank() {
-		System.out.println("hi");
+	// @RequestMapping(value = "/bank", method = RequestMethod.POST, produces =
+	// { "application/json" })
+	@PostMapping("/bank")
+	public int addBank(@RequestBody BankMst bankMst) {
 
-		return bankMstService.getBankDetail();
+		return bankMstService.create(bankMst);
 
+	}
+
+	@PutMapping("/bank")
+	public void updateBank(@RequestBody BankMst bankMst) {
+		bankMstService.update(bankMst);
+	}
+
+	@DeleteMapping("/bank/{id}")
+	public void daleteBank(@PathVariable int id) {
+		bankMstService.delete(id);
+		System.out.println(id);
 	}
 }
