887114bf2e17a8a6aa6b10d5518d269278109a55
Govind Kumar Choudhary <govind.contact@gmail.com>
2018-07-17 10:54:37 +0530

    using @GetMapping

--- SpringJPAPostgreSQLAngularJS/src/main/java/com/rank/ccms/controller/BankMstController.java
+++ SpringJPAPostgreSQLAngularJS/src/main/java/com/rank/ccms/controller/BankMstController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -20,7 +21,8 @@ public class BankMstController {
 	@Autowired
 	BankMstService bankMstService;
 
-	@RequestMapping(value = "/bank", method = RequestMethod.GET, produces = { "application/json" })
+	//@RequestMapping(value = "/bank", method = RequestMethod.GET, produces = { "application/json" })
+	@GetMapping("/bank")
 	public List<BankMst> getAllEmployee() {
 		System.out.println("hi");
 
